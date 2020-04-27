aa82dbc68d961dfae349cce353a57ee72ff8c5c3
Yee Poh <yee.poh@vonage.com>
2020-04-27 10:45:56 +0800

    remove latin1 to utf8 conversion, csv should already be in utf8

--- src/services/sms/index.js
+++ src/services/sms/index.js
@@ -1,35 +1,28 @@
-const windows1252 = require('windows-1252');
-const utf8 = require('utf8');
-
 // eslint-disable-next-line no-control-regex
 const isUnicode = text => /[^\u0000-\u00ff]/.test(text);
 
-const convertToUtf8 = (text) => {
-  if (text == null) {
-    return null;
-  }
-  const encoded = windows1252.encode(text);
-  const decoded = utf8.decode(encoded);
-  return decoded;
-};
-
 const sendSms = (from, to, text, apiKey, apiSecret, apiUrl, campaignName, axios) => {
-  const sanitizedText = convertToUtf8(text);
-  const sanitizedType = isUnicode(sanitizedText) ? 'unicode' : 'text';
+  // Determine proper type to send as
+  const type = isUnicode(text) ? 'unicode' : 'text';
+
+  // Constructing the API Request Body
   const body = {
     api_key: apiKey,
     api_secret: apiSecret,
     from,
     to,
-    text: sanitizedText,
-    type: sanitizedType,
+    text,
+    type,
     'client-ref': campaignName,
   };
 
   return axios.post(apiUrl, body)
     .catch((error) => {
+      // Check for 429: Too Many Requests
       if (error.response != null && error.response.status === 429) {
         console.log('Too many request (429) detected, put back into queue');
+
+        // Recursively call self, to put request back into queue
         return sendSms(from, to, text, apiKey, apiSecret, apiUrl, campaignName, axios);
       }
 
